acd8951e26f6b37c7e3c9ebbc4ba21a04c147773
Rebecca Tong <rtong@buffalo.edu>
2018-03-17 04:30:52 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,6 +22,6 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, SUM(character_books.book_id) FROM books JOIN character_books
+  "SELECT characters.name, SUM(character_books.book_id) FROM characters JOIN character_books
   ON character_books.book_id = books.id "
 end
