8fe916ec9c8f6dadceaa00ed5742029f9381c959
Rebecca Tong <rtong@buffalo.edu>
2018-03-17 04:04:41 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,8 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, SUM(characters.species) FROM characters GROUP BY characters.species"
+  "SELECT characters.species, SUM(characters.species) FROM characters GROUP BY characters.species
+  ORDER BY SUM(characters.species)"
 end
 
 def select_name_and_series_subgenres_of_authors
