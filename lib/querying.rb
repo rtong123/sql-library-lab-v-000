62ba6341a3ebe98ccb044263442e984cb2fa7c3a
Rebecca Tong <rtong@buffalo.edu>
2018-03-17 04:19:25 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,8 +13,8 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, series.subgenre_id FROM series JOIN subgenres
-  ON series.subgenre_id = subgenres.id;"
+  "SELECT authors.name, series.subgenre_id FROM series JOIN authors
+  ON series.author_id = authors.id;"
 end
 
 def select_series_title_with_most_human_characters
