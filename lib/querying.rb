ca30fd219030fd384f54f21fe6e51b53c52f67bc
Rebecca Tong <rtong@buffalo.edu>
2018-03-17 04:35:10 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,10 +18,10 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series JOIN "
+  "SELECT series.title, COUNT(characters.species) FROM characters JOIN series
+  ON characters.series_id = series.id"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, SUM(character_books.book_id) FROM characters JOIN character_books
-  ON character_books.book_id = books.id "
+  "SELECT characters.name, SUM()"
 end
