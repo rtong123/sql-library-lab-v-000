11dad24070d85584ca72af44e45b677ca15a6912
Rebecca Tong <rtong@buffalo.edu>
2018-03-17 04:42:42 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series JOIN characters
+  "SELECT series.title FROM authors JOIN characters
   ON characters.author_id = authors.id WHERE characters.species = 'human'
   GROUP BY series.title ORDER BY COUNT(characters.species) DESC LIMIT 1"
 end
