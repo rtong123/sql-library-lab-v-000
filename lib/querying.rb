c35f5aa3f132f53ec47191bc0486898c68b133d6
Rebecca Tong <rtong@buffalo.edu>
2018-03-17 04:27:21 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,7 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM series JOIN authors
-  ON series.author_id = authors.id JOIN subgenres ON series.subgenres_id = subgenres.id;"
+  ON series.author_id = authors.id JOIN subgenres ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
